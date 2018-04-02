1a5f4bb823d984bf21cc369701e44261a4598fe0
F4raday <bnbyar@gmail.com>
2018-04-02 16:58:16 +0300

    twitter stream filtering, not really good

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,23 +1,50 @@
+import java.io.FileInputStream
+import java.util.Properties
+
 import org.apache.spark.streaming.twitter.TwitterUtils
 import org.apache.spark.streaming._
 import org.apache.spark.SparkConf
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
+import scala.collection.JavaConverters._
 
 object Main extends App {
 
-        System.setProperty("twitter4j.oauth.consumerKey", "CONSUMER_KEY")
-        System.setProperty("twitter4j.oauth.consumerSecret", "CONSUMER_SECRET")
-        System.setProperty("twitter4j.oauth.accessToken", "ACCESS_TOKEN")
-        System.setProperty("twitter4j.oauth.accessTokenSecret", "ACCESS_TOKEN_SECRET")
+        try {
+                val properties = new Properties()
+                properties.load(new FileInputStream("src/main/properties/twitter4j.properties"))
+
+                properties.entrySet().asScala.foreach((entry) => (sys.props += ((entry.getKey.asInstanceOf[String], entry.getValue.asInstanceOf[String]))))
+        } catch {
+                case e: Exception =>
+                        e.printStackTrace()
+                        sys.exit(1)
+        }
+
+        Logger.getLogger("org").setLevel(Level.OFF)
+        Logger.getLogger("akka").setLevel(Level.OFF)
 
         val sc = new SparkConf().setAppName("BravoML").setMaster("local[2]") // local
         val ssc = new StreamingContext(sc, Seconds(15))
         val stream = TwitterUtils.createStream(ssc, None)
 
 
-        case class Tweet(createdAt:Long, text:String)
-        val twits = stream.window(Seconds(60)).map(m=>
-        Tweet(m.getCreatedAt().getTime()/1000, m.toString)
-        )
+        case class Tweet(createdAt: Long, text: String)
+
+        val twits = stream.window(Seconds(60))
+          .filter((tweet) =>
+                  tweet.getLang == "en"
+//            &&
+//                    (tweet.getHashtagEntities()
+//            .map(he => he.getText.toLowerCase())
+//            .contains("usa")
+//            ||
+//            tweet.getText.split(" ").map(word => word.toLowerCase).contains("usa")
+//            )
+          )
+          .map(m => Tweet(m.getCreatedAt().getTime() / 1000, m.getText)
+          )
 
 
         twits.foreachRDD(rdd => rdd.collect().foreach(println))
